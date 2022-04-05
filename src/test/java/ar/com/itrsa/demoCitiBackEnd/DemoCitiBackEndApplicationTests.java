cbc772200d76c5e4a151038c12298c456aed9713
Matias Gonzalez <matias_gonzalez@itrsa.com.ar>
2022-04-05 17:49:45 -0300

    first commit

---
+++ src/test/java/ar/com/itrsa/demoCitiBackEnd/DemoCitiBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.itrsa.demoCitiBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCitiBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
